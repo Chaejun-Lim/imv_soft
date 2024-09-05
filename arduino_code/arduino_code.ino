28e7a47c4b919a19d837508d59d8bcc9890237c7
Chaejun-Lim <chajun1018@gmail.com>
2024-09-05 20:53:24 +0900

    Add files via upload

---
+++ arduino_code/arduino_code.ino
@@ -0,0 +1,25 @@
+#define MOTORA_A 10
+#define MOTORA_B 6
+
+void setup() {
+  pinMode(MOTORA_A, OUTPUT);
+  pinMode(MOTORA_B, OUTPUT);
+  Serial.begin(9600);
+}
+int soil_moisture = analogRead(A0);
+int light = analogRead(A1);
+int data_1,data_2;
+void loop() {
+  data_1 = soil_moisture;
+  data_2 = light;
+  Serial.print(data_1);
+  Serial.print(",");
+  Serial.println(data_2);
+  if (data_1<450) {
+    digitalWrite(MOTORA_A,HIGH);
+    digitalWrite(MOTORA_B,LOW);
+    delay(1700);
+    digitalWrite(MOTORA_A,LOW);
+  }
+  delay(1000);
+}
